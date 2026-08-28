a6ec3d619fb6caa01a27f165c4ce4ed2e4620418
ajayrathod <ajayrathodddd1@gmail.com>
2026-08-28 22:45:51 +0530

    Fix post comments route

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -7,7 +7,7 @@ const { getPosts, getUserPosts, createPost, addComment, toggleLike, deletePost, 
 router.route("/").get(protect, getPosts).post(protect, upload.single("image"), createPost);
 router.route("/user/:id").get(protect, getUserPosts);
 router.route("/saved").get(protect, getSavedPosts);
-router.route("/:id/comment").post(protect, addComment);
+router.route("/:id/comments").post(protect, addComment);
 router.route("/:id/like").post(protect, toggleLike);
 router.route("/:id/save").post(protect, savePost).delete(protect, unsavePost);
 router.route("/:id").delete(protect, deletePost);
